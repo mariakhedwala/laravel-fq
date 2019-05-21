b159217c0153e2a4cb2f07d93333bda3f0746ceb
pajinkyaprdxn <ajinkyap@prdxn.com>
2019-05-21 17:10:01 +0530

    Crud operations on controller created with respective view

--- laravel-crud/app/Http/Controllers/UserController.php
+++ laravel-crud/app/Http/Controllers/UserController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\User;
+use App\JobTitle;
+use App\Country;
+use App\City;
 
 class UserController extends Controller
 {
@@ -23,9 +26,12 @@ class UserController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function create(User $user, JobTitle $job_title, Country $country, City $city)
     {
-        //
+        $job_titles = $job_title->getJobs();
+        $cities = $city->getCities();
+        $countries = $country->getCountries();
+        return view('user.create', compact('user', 'job_titles', 'countries', 'cities'));
     }
 
     /**
@@ -36,7 +42,41 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        try {
+            $validated = $request->validate([
+                'name' => ['required','max:255'],
+                'email' => ['required','email'],
+                'job_title' => ['nullable', 'string'],
+                'city' => ['nullable', 'string'],
+                'country' => ['nullable', 'string'],
+                'password' => ['required', 'confirmed', 'min:4', 'max:10'],
+                'password_confirmation' => ['required', 'min:4'],
+            ]);
+
+            if ($validated) {
+                $createUser = new User;
+                $createUser = $createUser->createUsers($validated);
+                
+                if ($createUser) {
+                    $request->session()->flash('success', 'Contact created');
+                    return redirect('users');
+                }else {
+                    $request->session()->flash('danger', 'Contact creation failed');
+                    return redirect('users.create');
+                }
+            } else {
+                return redirect()->route('users.create')->with('error');
+            }
+        } catch (\Illuminate\Database\QueryException $e) {
+            $errorCode = $e->errorInfo[1];
+            if ($errorCode == '1062') {
+                return redirect()->back()->withErrors(['email' => 'Email ID already exist']);
+            } else {
+                return redirect()->back()->with('error');
+            }
+        } catch(Exception $e) {
+            return redirect()->back()->with('error');
+        }
     }
 
     /**
@@ -56,9 +96,12 @@ class UserController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(User $user, JobTitle $job_title, Country $country, City $city)
     {
-        //
+        $job_titles = $job_title->getJobs();
+        $cities = $city->getCities();
+        $countries = $country->getCountries();
+        return view('user.create', compact('user', 'job_titles', 'countries', 'cities'));
     }
 
     /**
@@ -70,8 +113,51 @@ class UserController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $id = (int) $id;
+        try {
+            $validated = $request->validate([
+                'name' => ['required', 'max:255'],
+                'email' => ['required', 'email', "unique:users,email,$id"],
+                'job' => ['nullable', 'string'],
+                'city' => ['nullable', 'string'],
+                'country' => ['nullable', 'string'],
+                'password' => ['nullable', 'confirmed', 'min:4', 'max:8'],
+                'password_confirmation' => ['nullable', 'min:4'],
+            ]);
+            $user = User::findOrFail($id);
+            $newPassword = $request->get('password');
+
+            if ($validated) {
+                if (empty($newPassword)) {
+                    $updateUser = $user->update($request->except('password'));
+                } else {
+                    $updateUser = $user->editUser($validated);
+                }
+
+                if ($updateUser == true) {
+                    $request->session()->flash('success', 'Contact updated');
+                    return redirect('users');
+                } else {
+                    $request->session()->flash('danger', 'Contact update failed');
+                    return redirect('users');
+                }
+            } else {
+                return redirect()->route('users.create')->with('error');
+            }
+            return redirect('users');
+
+        } catch (\Illuminate\Database\QueryException $e) {
+            $errorCode = $e->errorInfo[1];
+            if ($errorCode == '1062') {
+                return redirect()->back()->withErrors(['email' => 'Email ID already exist']);
+            } else {
+                return redirect()->back()->with('error');
+            }   
+        } catch (Exception $e) {
+            return redirect()->back()->with('error');
+        }
     }
+
 
     /**
      * Remove the specified resource from storage.
@@ -79,8 +165,18 @@ class UserController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Request $request, User $user)
     {
-        //
+        $delete = $user->delete();
+
+        if ($delete) {
+            $request->session()->flash('success', 'Contact deleted');
+            return redirect('users');
+        } else {
+            $request->session()->flash('danger', 'Contact delete failed');
+            return redirect('users');
+        }
+
+        return redirect('users');
     }
 }
---
+++ laravel-crud/resources/views/user/create.blade.php
@@ -0,0 +1,122 @@
+@extends('layouts.app')
+
+@section('content')
+<section class="create">
+    <div class="wrapper">
+        <h2>
+            {{ $h2value = $user->id ? 'Update' : 'Register' }}
+        </h2>
+        <form method="POST" action="{{ $action = $user->id ? "/users/$user->id" : "/users" }}" id="login" novalidate
+            autocomplete="off">
+            @csrf
+            @method($method = $user->id ? 'PATCH' : 'POST')
+            <div class="form-group">
+                <label for="name">{{ __('Name') }}</label>
+                <div class="form-group__name">
+                    <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}"
+                        name="name" value="{{ $name = $user->id ? $user->name : old('name') }}" autofocus="true">
+                    @error('name')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label for="email">{{ __('Email') }}</label>
+                <div class="form-group__email">
+                    <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}"
+                        name="email" value="{{ $email = $user->id ? $user->email : old('email') }}" autofocus="true">
+
+                    @error('email')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label for="job">{{ __('Job Title') }}</label>
+                <div class="form-group__job">
+                    <select name="job" id="job" class="form-control{{ $errors->has('job_title') ? ' is-invalid' : '' }}"
+                        autofocus="true">
+                        <option selected value="{{ $job_title = $user->id ? $user->job_title : old('job_title') }}">
+                            {{ $job_title = $user->job_title ? $user->job_title : 'Select job_title' }}</option>
+                        @foreach ($job_titles as $job_title)
+                        <option value="{{ $job_title->job_title }}">{{ $job_title->job_title }}</option>
+                        @endforeach
+                    </select>
+                    @error('job_title')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label for="city">{{ __('City') }}</label>
+                <div class="form-group__city">
+                    <select name="city" id="city" class="form-control{{ $errors->has('city') ? ' is-invalid' : '' }}"
+                        autofocus="true">
+                        <option selected value="{{ $city = $user->id ? $user->city : old('city') }}">
+                            {{ $city = $user->city ? $user->city : 'Select city' }}</option>
+                        @foreach ($cities as $city)
+                        <option value="{{ $city->city }}">{{ $city->city }}</option>
+                        @endforeach
+                    </select>
+                    @error('city')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label for="country">{{ __('Country') }}</label>
+                <div class="form-group__country">
+                    <select name="country" id="country"
+                        class="form-control{{ $errors->has('country') ? ' is-invalid' : '' }}" autofocus="true">
+                        <option selected value="{{ $country = $user->id ? $user->country : old('country') }}">
+                            {{ $country = $user->country ? $user->country : 'Select country' }}</option>
+                        @foreach ($countries as $country)
+                        <option value="{{ $country->country }}">{{ $country->country }}</option>
+                        @endforeach
+                    </select>
+                    @error('country')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label for="password">{{ __('Password') }}</label>
+
+                <div class="form-group__password">
+                    <input id="password" type="password"
+                        class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password"
+                        autofocus="true">
+
+                    @error('password')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label for="confirmPassword">{{ __('Confirm Password') }}</label>
+
+                <div class="form-group__password">
+                    <input id="confirmPassword" type="password"
+                        class="form-control{{ $errors->has('password_confirmation') ? ' is-invalid' : '' }}"
+                        name="password_confirmation" autofocus="true">
+                    @error('password_confirmation')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
+                </div>
+            </div>
+
+            <div class="form-group">
+                <button id="create-user" type="submit" class="btn btn-primary">
+                    {{ $valuebtn = $user->id ? 'Update' : 'Submit' }}
+                </button>
+            </div>
+        </form>
+    </div>
+</section>
+@endsection
